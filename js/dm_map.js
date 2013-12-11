abcb0873804393dfa5443b3ad2c2055d8bed6d4f
volgoweb <volgoweb@bk.ru>
2013-12-11 11:50:01 +0400

    Добавил поиск объекта на карте по nid. ДОбавил корректировку масштаба.

--- js/dm_map.js
+++ js/dm_map.js
@@ -1,30 +1,35 @@
 Drupal.dfunc_map = {};
+
+Drupal.dfunc_map.config = {
+  base_zoom: 15,
+};
+
 Drupal.dfunc_map.map = {};
+
 Drupal.dfunc_map.myCollection = {};
+
+/**
+ * Отображает на карте указанные объекты
+ */
 Drupal.dfunc_map.show_objects = function(objects) {
 	var map = Drupal.dfunc_map.map;
 	var myCollection = Drupal.dfunc_map.myCollection;
 
   myCollection.removeAll();
 
-  // if (!(objects instanceof  Array)) {
-  //   return;
-  // }
-
   var number = 1;
-  var src_res = '';
   for (i in objects) {
     var placemark = new ymaps.Placemark([objects[i].lat,objects[i].lon], {
-      iconContent: number,
-        balloonContentHeader: '<div style="color:#ff0303;font-weight:bold">'+objects[i].title+'</div>',
-        balloonContentBody: '<div style="font-size:13px;"><div><strong>Адрес:</strong> '+objects[i].title + '</div></div>'   
-            }, {
-            // Опции
-            preset: 'twirl#nightStretchyIcon' // иконка растягивается под контент
-            });
+        iconContent: number,
+        balloonContentHeader: '<div class="balloon__header">'+objects[i].title+'</div>',
+        balloonContentBody: '<div class="balloon__body">'+objects[i].address + '</div>',
+        nid: objects[i].nid,   
+      }, {
+        // Опции
+        preset: 'twirl#nightStretchyIcon' // иконка растягивается под контент
+      });
 
     myCollection.add(placemark);		
-    src_res=src_res+'<p>'+number+'. '+'<a href="#" onClick="return Drupal.dfunc_map.go_to('+objects[i].lat+', '+objects[i].lon+",'"+objects[i].title+"');"+'\">'+objects[i].title+'</a></p>';
 
     number++;
   }
@@ -32,23 +37,31 @@ Drupal.dfunc_map.show_objects = function(objects) {
 
   // Set center and zoom using collection bounds.
   map.setBounds(myCollection.getBounds());
-
+  
+  Drupal.dfunc_map.correct_zoom();
 };
 
-Drupal.dfunc_map.go_to = function(lat, lon, title) {
-	var map = Drupal.dfunc_map.map;
-	var myCollection = Drupal.dfunc_map.myCollection;
+/**
+ * Переводит фокус карты на объект
+ */
+Drupal.dfunc_map.go_to = function(lat, lon, nid) {
+	var map          = Drupal.dfunc_map.map,
+      myCollection = Drupal.dfunc_map.myCollection,
+      config       = Drupal.dfunc_map.config;
 
-  map.setCenter([lat, lon], 13);
+  map.setCenter([lat, lon], config.base_zoom);
 
   myCollection.each(function (item) {
-    if (item.properties.get('balloonContentHeader') == '<div style="color:#ff0303;font-weight:bold">'+title+'</div>') {
+    if (item.properties.get('nid') == nid) {
       item.balloon.open();
     }
   });
   return false;
 };
 
+/**
+ * Вешает обработчики для списка объектов
+ */
 Drupal.dfunc_map.bind_objects_items = function() {
   $('.map-object-item_js').bind('click', function(event) {
     event.preventDefault();
@@ -57,7 +70,8 @@ Drupal.dfunc_map.bind_objects_items = function() {
     var lat = $(this).attr('lat');
     var lon = $(this).attr('lon');
     var title = $(this).text();
-    Drupal.dfunc_map.go_to(lat, lon, title)
+    var nid = $(this).attr('nid');
+    Drupal.dfunc_map.go_to(lat, lon, nid);
   });
 };
 
@@ -66,11 +80,12 @@ Drupal.behaviors.dm_map = function() {
     return;
   }
 
+  var config = Drupal.dfunc_map.config;
 	
 	ymaps.ready(function () {
     Drupal.dfunc_map.map = new ymaps.Map('services-ymap', {
         center: [56.326944, 44.0075],
-        zoom: 15,
+        zoom: config.base_zoom,
         type: 'yandex#map',
         behaviors: ['default', 'scrollZoom']
     });
@@ -87,4 +102,17 @@ Drupal.behaviors.dm_map = function() {
 
     Drupal.dfunc_map.bind_objects_items();
   })
+};
+
+/**
+ * При слишком маленьком масштабе увеличивает его
+ */
+Drupal.dfunc_map.correct_zoom = function() {
+	var map    = Drupal.dfunc_map.map,
+      zoom   = map.getZoom();
+      config = Drupal.dfunc_map.config;
+
+  if (zoom > config.base_zoom) {
+    map.setZoom(config.base_zoom);
+  }
 };
