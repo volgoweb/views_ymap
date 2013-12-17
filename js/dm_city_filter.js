af63488b93ba5a94c63b23df8a655c6a7c7d46d5
volgoweb <volgoweb@bk.ru>
2013-12-17 17:22:35 +0400

    Добавил блок со всеми городами

---
+++ js/dm_city_filter.js
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+
+Drupal.behaviors.dm_city_filter = function (context) {
+  var $form = $('#views-exposed-form-map-page-1'), 
+      $city_input = $form.find('input[name="city"]');
+  $city_input.bind('click', function() {
+    if ($('.cities-links').length > 0) {
+      $('.cities-links').remove();
+    }
+    else {
+      show_cities_block();
+    }
+  });
+
+  $city_input.bind('keydown', function() {
+    $('.cities-links').remove();
+  });
+
+
+  function show_cities_block() {
+    var block = Drupal.settings.dfunc_map.cities_links_block;
+    var top = $city_input.position().top;
+    var height = $city_input.height();
+    $city_input.after(block);
+    $('.cities-links').css({
+      position: 'absolute',
+      top: (top + height) + 'px',
+      'z-index': 1000,
+    });
+
+    $('.cities-links .close').bind('click', function() {
+      $('.cities-links').remove();
+    });
+  }
+
+  function get_current_country_name() {
+    var $country_select = $form.find('select[name="country"]');
+    return $country_select.find('option:selected').text();
+  }
+
+};
